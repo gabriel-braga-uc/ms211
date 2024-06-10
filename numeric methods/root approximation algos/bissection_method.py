45c101d886a446d2388813d6358a5269368a7c8b
Gabriel Braga <126345318+gabriel-braga-uc@users.noreply.github.com>
2024-06-10 16:39:32 -0300

    Update bissection_method.py

--- numeric methods/root approximation algos/bissection_method.py
+++ numeric methods/root approximation algos/bissection_method.py
@@ -1,5 +1,3 @@
-## This method approximates the root of a continuous function in a initial, pre determined, interval [a, b] by splitting this interval in half while still containing the real root in it, the size of the interval determines the precision of the approximation.
-## Ideally the initial interval [a, b] should contain no more than 1 root and it should be as small as reasonably possible initially for better results.
 import matplotlib.pyplot as plt
 
 ##Tweaks##
